3ef93fcd17749e264d7524335f94701170965030
abedshamia <ab.shamia96@gmail.com>
2022-03-01 23:50:17 +0200

    renamde server to index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const app = require('./app');
 
 // Start the server
-app.set('port', process.env.PORT || 3000);
-const PORT = app.get('port');
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
