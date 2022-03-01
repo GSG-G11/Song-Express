af46ce70da413c0fb69338c96d14035da3f85efd
Abedalrahman Shamia <73759748+abedshamia@users.noreply.github.com>
2022-03-01 15:49:19 +0200

    Revert "Add Basic Server Setup and Options file"

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-
-// Start the server
-app.set('port', process.env.PORT || 3000);
-const PORT = app.get('port');
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
