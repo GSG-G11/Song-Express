dff984a0f0db2e98dd85574c69124a12427b06bc
abedshamia <ab.shamia96@gmail.com>
2022-03-02 00:08:10 +0200

    fix package json

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const axios = require('axios').default;
 const options = require('./options');
 
-const errorRouter = require('./routes/index.js');
+const errorRouter = require('./routes/index');
 app.use(express.static('public'));
 
 app.get('/search', (req, res) => {
