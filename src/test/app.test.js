e01cda59b4e1ebe3980845e31045b26473c29e75
abedshamia <ab.shamia96@gmail.com>
2022-03-02 13:18:10 +0200

    Add more tests to test file

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,27 +1,66 @@
-const request = require("supertest");
-const { response } = require("../app");
-const app = require("../app");
+const request = require('supertest');
+const app = require('../app');
 
-test("should response with the get method", () => {
+test('Should respond with the get method', done => {
   request(app)
-    .get("/")
+    .get('/')
     .expect(200)
-    .expect("Content-Type", /html/)
-    .end((error) => {
-      if (error) {
-        console.log(error);
-      }
+    .expect('Content-Type', /html/)
+    .end((error, res) => {
+      if (error) done(error);
+      expect(res.statusCode).toBe(200);
+      done();
     });
 });
 
-test("testing home page", () => {
+test('Should respond with homepage', done => {
   request(app)
-    .get("/home")
+    .get('/home')
     .expect(200)
-    .expect("Content-Type", /html/)
-    .end((error) => {
-      if (error) {
-        console.log(error)
-      }
+    .expect('Content-Type', /html/)
+    .end((error, res) => {
+      if (error) done(error);
+      expect(res.statusCode).toBe(200);
+      expect(res.text).toContain('Results');
+      done();
+    });
+});
+
+test('Should get results', done => {
+  request(app)
+    .get('/search?term=test')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((error, res) => {
+      if (error) done(error);
+      expect(res.statusCode).toBe(200);
+      expect(res.body.tracks.hits.length).toBeGreaterThan(0);
+      done();
+    });
+});
+
+test('Should get No results found', done => {
+  request(app)
+    .get('/search?term=asdfasdfasdsad')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((error, res) => {
+      if (error) done(error);
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toEqual({});
+      done();
+    });
+});
+
+test('Should respond with 404 not found page', done => {
+  request(app)
+    .get('/potato')
+    .expect(404)
+    .expect('Content-Type', /html/)
+    .end((error, res) => {
+      if (error) done(error);
+      expect(res.text).toContain('404');
+      expect(res.statusCode).toBe(404);
+      done();
     });
 });
